cee785e82e192e15f35a723e8408c783f7fc3a15
Naeefffaisal1 <69986511+Naeefffaisal1@users.noreply.github.com>
2023-12-05 00:28:30 +0300

    Update links.js

--- links.js
+++ links.js
@@ -1,7 +1,8 @@
 let links = [
 
 "https://wa.me/918075363538",    
-"https://wa.me/919074841077", 
+"https://wa.me/919074841077",
+"https://wa.me/918921979891",
          
      // Add links like this
 ];
